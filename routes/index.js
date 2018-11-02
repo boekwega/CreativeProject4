774c3aacefe1177c4c8f650687e648e4ee606549
AbbyB <boekwega@gmail.com>
2018-11-02 04:52:31 +0000

    sending a single name

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+// var request = require('request');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//   res.render('index', { title: 'Express' });
+    res.sendFile('names.html', {root: 'public'});
 });
 
+/* get name route */
 router.get("/getname", function(req, res, next){
+    
     console.log("in getname route");
+    
+    fs.readFile(__dirname + '/names.dat.txt', function(err, data) {
+        if(err) throw err;
+        var names = data.toString().split("\n");
+        var jsonresult = [];
+        var result = names[1];
+        if(result != -1) {
+            jsonresult.push({name: result});
+        }
+        
+        res.status(200).json(jsonresult);
+    })
 })
 
 module.exports = router;
